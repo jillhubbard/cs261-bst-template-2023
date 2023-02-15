b0d67b6a21fbfd0598076ef1cfdb1803e27918a8
Jill Hubbard <jill.m.hubbard@gmail.com>
2023-02-15 04:20:47 -0800

    initial commit

---
+++ bst.py
@@ -0,0 +1,13 @@
+# BinarySearchTree: A binary search tree.
+# Implement as many operations as possible with recursion.
+# If you can't figure it out recursively, use a loop. (But then refactor
+# your implementation into a recursive one!)
+# Your implementation should pass the tests in test_bst.py.
+
+# Name:
+# Collaborators:
+# Time spent:
+
+
+class FixMe:
+    pass
